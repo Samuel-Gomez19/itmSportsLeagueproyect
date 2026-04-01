d10d7ef0afa5d51e8cb2f52d3344f0650c10b76d
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-01 00:47:00 -0500

    create a enum called SponsorCategory

---
+++ SportsLeague.Domain/enums/SponsorCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.enums
+{
+    public enum SponsorCategory
+    {
+        Main =0,
+        First=1,
+        Second=2,
+        Third = 3,
+
+    }
+}
