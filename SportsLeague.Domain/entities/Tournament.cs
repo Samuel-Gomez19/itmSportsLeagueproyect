69c4d9ccf7dcd5197e25b0441da8c8647b21611d
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-03-31 19:21:55 -0500

    add navigation properties

--- SportsLeague.Domain/entities/Tournament.cs
+++ SportsLeague.Domain/entities/Tournament.cs
@@ -20,7 +20,7 @@ namespace SportsLeague.Domain.entities
         // Navigation Properties
 
         public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
-        public ICollection<TournamentSponsor> TournamentSponsor { get; set; }= new List<TournamentSponsor>();
+        public ICollection<TournamentSponsor> TournamentSponsor { get; set; } = new List<TournamentSponsor>();
 
     }
 }
