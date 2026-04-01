b445a7f0b2df24feed6d442d17a89de9f3c6dae8
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-01 00:59:30 -0500

    Create Entity with the navigation propierties

---
+++ SportsLeague.Domain/entities/TournamentSponsor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.entities
+{
+    public class TournamentSponsor:AuditBase//Creamos la clase sponsor con los atributos de la auditbase
+    {
+        public int TournamentId { get; set; }
+        public int SponsorId { get; set; }
+        public decimal ContractAmount { get; set; }
+
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+
+
+        //Agregamos propiedades de navegacion
+        public Sponsor Sponsor { get; set; } = null!;
+        public Tournament Tournament { get; set; } = null!;
+
+    }
+}
