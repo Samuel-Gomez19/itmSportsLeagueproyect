1110a72371d77089f8a2d76dd7c3c175dce154ec
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-01 23:51:38 -0500

    create the dto sponsor request with what he expect

---
+++ SportsLeague.API/DTOs/Response/SponsorResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using SportsLeague.API.DTOs.Request;
+using SportsLeague.Domain.enums;
+
+namespace SportsLeague.API.DTOs.Response
+{
+    public class SponsorResponseDTO
+    {
+       public int SponsorId {  get; set; }
+        public string SponsorName { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string? WebSiteURl { get; set; }
+
+        public SponsorCategory Category { get; set; }
+        public int TournamentId { get; set; }
+        public string TournamentName { get; set; }= string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
