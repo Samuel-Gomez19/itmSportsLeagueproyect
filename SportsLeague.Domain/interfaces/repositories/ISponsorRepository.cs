5572d6b2252511a54379218dca73827242f77548
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-01 23:07:04 -0500

    create the repository that inherit teh crud methods from the generic

--- SportsLeague.Domain/interfaces/repositories/ISponsorRepository.cs
+++ SportsLeague.Domain/interfaces/repositories/ISponsorRepository.cs
@@ -5,6 +5,8 @@ namespace SportsLeague.Domain.interfaces.repositories
     public interface ISponsorRepository : IGenericRepository<Sponsor>// creo el IRepository donde hereda los metodos del generic 
     {
         Task<Sponsor?> ExistByNameAsync(string SponsorName);//para ver si es repetido el nombre del sponsor 
+        
         Task<Sponsor?> GetByIdWithTournamentAsync(int Id);
+        Task AddToTournamentAsync(int tournamentId, int sponsorId);
     }
 }
