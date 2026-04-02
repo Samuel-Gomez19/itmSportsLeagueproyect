e322e6e88484f0e8c4e4937b251df2b0c2380272
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-01 23:51:16 -0500

    create the sponsor dto request with whta it have

---
+++ SportsLeague.API/DTOs/Request/SponsorRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using SportsLeague.Domain.enums;
+
+namespace SportsLeague.API.DTOs.Request
+{
+    public class SponsorRequestDTO
+    {
+        
+        public string SponsorName { get; set; } = String.Empty;
+        public string ContactEmail { get; set; } = String.Empty;
+        public string? Phone { get; set; }
+        public string? WebSiteURl { get; set; }
+
+        public SponsorCategory Category { get; set; }
+
+    }
+}
