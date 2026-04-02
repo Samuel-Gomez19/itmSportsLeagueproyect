b91a3a970424243be6c662ec1115756cd335b1c5
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-04-02 13:31:40 -0500

    create the iservice, that include the crud from the generic repository

--- SportsLeague.Domain/interfaces/services/ISponsorService.cs
+++ SportsLeague.Domain/interfaces/services/ISponsorService.cs
@@ -9,13 +9,13 @@ namespace SportsLeague.Domain.interfaces.services
     public interface ISponsorService//CReamos el iservice de sponsor primero con sus acciones propias de la aplicacion
     {
         Task<IEnumerable<Sponsor>> GetAllAsync();
-        Task<Sponsor?> GetByidAsync(string id);
+        Task<Sponsor?> GetByIdAsync(int id);
         Task<Sponsor> CreateAsync(Sponsor sponsor);
         Task UpdateAsync(int id, Sponsor sponsor);
         Task DeleteAsync(int id);
 
         Task UpdateCategoryAsync(int id, SponsorCategory newCategory);//Permite subir de categoria de patrocinador
         Task AddToTournamentAsync(int tournamentId, int sponsorId);//permite asociar sponsor a torneo 
-
+        
     }
 }
