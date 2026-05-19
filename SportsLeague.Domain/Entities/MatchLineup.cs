26e2e40a5ab423eb90ed4b4def6222bf663a6602
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-05-19 00:06:13 -0500

    Create the new entitie

---
+++ SportsLeague.Domain/Entities/MatchLineup.cs
@@ -0,0 +1,21 @@
+﻿using SportsLeague.Domain.entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class MatchLineup:AuditBase
+    {
+        public int MatchId { get; set; }//FK a match
+        public int PlayerId { get; set; }// Fk a Player
+        public bool IsStarter { get; set; }
+        public string PlayerPosition { get; set; } = string.Empty;
+
+        // navigation propierties
+
+        public Match Match { get; set; } = null!;
+        public Player Players { get; set; } = null!;
+
+    }
+}
