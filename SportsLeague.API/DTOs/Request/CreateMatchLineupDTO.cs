929e51e6a80c842f5bd963c9aedf30cffbf00b79
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-05-21 16:56:28 -0500

    Create the Request repository

---
+++ SportsLeague.API/DTOs/Request/CreateMatchLineupDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsLeague.API.DTOs.Request
+{
+    public class CreateMatchLineupDTO//creamos el que le pedira al usuario los datos de su jugador 
+    {
+        public int PlayerId { get; set; }
+        public bool IsStarter { get; set; }
+        public string PlayerPosition { get; set; } = string.Empty;
+    }
+}
