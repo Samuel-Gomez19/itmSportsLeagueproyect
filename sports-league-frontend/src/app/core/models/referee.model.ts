22756c6cf852b7ead6f87c6676ca81d14438c1c9
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-06-21 20:08:29 -0500

    phase 1

---
+++ sports-league-frontend/src/app/core/models/referee.model.ts
@@ -0,0 +1,27 @@
+export interface Referee { 
+
+  id: number; 
+
+  firstName: string; 
+
+  lastName: string; 
+
+  nationality: string; 
+
+  createdAt: string; 
+
+  updatedAt: string | null; 
+
+} 
+
+  
+
+export interface RefereeRequest { 
+
+  firstName: string; 
+
+  lastName: string; 
+
+  nationality: string; 
+
+} 
